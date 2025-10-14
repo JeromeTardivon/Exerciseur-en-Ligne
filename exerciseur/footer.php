48b7a9296325024b3129b9cda9f0c76ccfc9433f
BARRALLON CLEIA p2401552 <p2401552@univ-lyon1.fr>
2025-10-14 17:01:34 +0200

    add module footer

---
+++ exerciseur/footer.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Page Profil</title>
+</head>
+
+<body>
+    <footer>
+        <div>
+            <h3>
+                Réalisé par :
+            </h3>
+            <ul>
+                <li>Cléïa BARRALLON</li>
+                <li>Samy-Félipe CUESTAS</li>
+                <li>Bastien FELIX</li>
+                <li>Jérôme TARDIVON</li>
+            </ul>
+        </div>
+
+        <p>Copyright © - Tous droit réservés</p>
+
+
+        <div>
+            <h3>Coordonnées :</h3>
+
+            <p>IUT de l'université Lyon1</p>
+            <p>
+                71 rue Peter Fink
+                <br>
+                01000 Bourg-en-Bresse
+            </p>
+        </div>
+    </footer>
+
+</body>
+</html>
--- exerciseur/profil.php
+++ exerciseur/profil.php
@@ -1,1 +1,21 @@
-<?php
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Page Profil</title>
+</head>
+
+<body>
+    <header>
+
+    </header>
+
+    <aside>
+
+    </aside>
+    <main>
+
+    </main>
+
+</body>
+</html>
