ba484faad1106b9b86bb6efbeab63b6fd2c02303
Samy Cuestas <samyfelipe90@hotmail.com>
2025-10-14 17:42:48 +0200

    ajout header

--- exerciseur/index.php
+++ exerciseur/index.php
@@ -1,1 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Exerciseur en ligne</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <?php include 'modules/header.php' ?>
+    <main>
+    </main>
+    <?php include 'modules/footer.php' ?>
+</body>
+</html>
---
+++ exerciseur/modules/header.php
@@ -0,0 +1,14 @@
+
+<header>
+    <nav id="menu">
+        <ul>
+            <li><a href="/index.php">Accueil</a></li>
+            <li>Mes classes</li>
+            <li>Suivis</li>
+            <li>Récents</li>
+            <li>Mes Chapitres</li>
+            <li>A propos</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+</header>
