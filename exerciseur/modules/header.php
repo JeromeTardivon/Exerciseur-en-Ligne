9ca2710ae7ca23ffeb0c85be3810723c0da29342
Samy Cuestas <samyfelipe90@hotmail.com>
2025-10-21 17:29:14 +0200

    module header

--- exerciseur/modules/header.php
+++ exerciseur/modules/header.php
@@ -1,5 +1,15 @@
+<?php
 
+?>
 <header>
+    <div class="barHeader">
+        <img src="exercisor3000.png" alt="">
+        <p>Exerciseur en ligne</p>
+        <div class="buttonsHeader">
+            <a href="">Espace Professeurs</a>
+            <a href="">Profil</a>
+        </div>
+    </div>
     <nav id="menu">
         <ul>
             <li><a href="/index.php">Accueil</a></li>
