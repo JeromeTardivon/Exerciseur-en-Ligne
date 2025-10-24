4fc620126d0f64876459f7eaf9f22eb3dcf696bb
Jérôme Tardivon <jerome.tardivon@gmail.com>
2025-10-24 11:10:35 +0200

    Refactor database schema to use UUIDs & added logs to database
    Updated tables to use UUIDs as primary keys and modified various columns for better data integrity and constraints. Added logs to the database

--- database_creation.sql
+++ database_creation.sql
@@ -4,71 +4,78 @@ DROP TABLE IF EXISTS exercise;
 DROP TABLE IF EXISTS users, class, chapter;
 
 CREATE TABLE users (
-  id INT NOT NULL ,
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   name varchar(50),
   surname varchar (50),
-  mail varchar(50),
-  password varchar(20),
-  type varchar(20),
-  PRIMARY KEY (id)
+  mail varchar(100) NOT NULL UNIQUE,
+  password text NOT NULL,
+  type varchar(20) NOT NULL DEFAULT 'student',
+  created_at timestamptz NOT NULL DEFAULT now(),
+  updated_at timestamptz NOT NULL DEFAULT now()
 );
 
 
 CREATE TABLE class(
-  id INT NOT NULL,
-  name varchar(100),
-  PRIMARY KEY (id)
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  name varchar(100) NOT NULL,
+  created_at timestamptz NOT NULL DEFAULT now(),
+  updated_at timestamptz NOT NULL DEFAULT now()
 );
 
 CREATE TABLE chapter(
-  id INT NOT NULL,
-  visible BOOLEAN,
-  level INT,
-  title VARCHAR(100),
-  description VARCHAR(2000),
-  timelimit INT,
-  coef INT,
-  PRIMARY KEY (id)
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  visible BOOLEAN NOT NULL DEFAULT true,
+  level INT NOT NULL DEFAULT 0,
+  title VARCHAR(100) NOT NULL,
+  description text,
+  minutestimelimit INT, 
+  coef INT DEFAULT 1,
+  created_at timestamptz NOT NULL DEFAULT now(),
+  updated_at timestamptz NOT NULL DEFAULT now()
 );
 
 CREATE TABLE exercise(
-  id INT NOT NULL,
-  random BOOLEAN,
-  coef INT,
-  content TEXT,
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  random BOOLEAN NOT NULL DEFAULT false,
+  coef INT DEFAULT 1,
+  content TEXT NOT NULL,
   type INT NOT NULL,
-  id_chapter INT NOT NULL,
-  PRIMARY KEY (id),
+  id_chapter uuid NOT NULL,
+  created_at timestamptz NOT NULL DEFAULT now(),
+  updated_at timestamptz NOT NULL DEFAULT now(),
   CONSTRAINT FK_exercise__chapter FOREIGN KEY (id_chapter) REFERENCES chapter(id)
 );
 
 CREATE TABLE inclass(
-  id_user INT NOT NULL,
-  id_class INT NOT NULL,
-  responsible BOOLEAN,
+  id_user uuid NOT NULL,
+  id_class uuid NOT NULL,
+  responsible BOOLEAN NOT NULL DEFAULT false,
+  joined_at timestamptz NOT NULL DEFAULT now(),
   CONSTRAINT FK_inClass__user FOREIGN KEY (id_user) REFERENCES users(id),
-  CONSTRAINT FK_inclass__class FOREIGN KEY (id_class) REFERENCES class(id)
+  CONSTRAINT FK_inclass__class FOREIGN KEY (id_class) REFERENCES class(id),
+  PRIMARY KEY (id_user,id_class)
 );
 
 
 CREATE TABLE result(
-  id INT NOT NULL,
-  id_subject INT NOT NULL,
-  id_user INT NOT NULL,
-  id_exercise INT NOT NULL,
-  id_class INT,
-  CONSTRAINT FK_grade__chapter FOREIGN KEY (id_subject) REFERENCES chapter(id),
-  CONSTRAINT FK_grade__user FOREIGN KEY (id_user) REFERENCES users(id),
-  CONSTRAINT FK_grade__exercise FOREIGN KEY (id_exercise) REFERENCES exercise(id),
-  CONSTRAINT FK_grade__class FOREIGN KEY (id_class) REFERENCES class(id),
-  PRIMARY KEY (id)
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  id_subject uuid,
+  id_user uuid,
+  id_exercise uuid,
+  id_class uuid,
+  created_at timestamptz NOT NULL DEFAULT now(),
+  grade INT NOT NULL DEFAULT 0,
+  CONSTRAINT FK_result__chapter FOREIGN KEY (id_subject) REFERENCES chapter(id) ON DELETE SET NULL,
+  CONSTRAINT FK_result__user FOREIGN KEY (id_user) REFERENCES users(id) ON DELETE SET NULL,
+  CONSTRAINT FK_result__exercise FOREIGN KEY (id_exercise) REFERENCES exercise(id) ON DELETE SET NULL,
+  CONSTRAINT FK_result__class FOREIGN KEY (id_class) REFERENCES class(id) ON DELETE SET NULL
 );
 
 CREATE TABLE course(
-  id INT NOT NULL,
-  id_chapter INT NOT NULL,
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  id_chapter uuid NOT NULL,
   content TEXT,
-  PRIMARY KEY (id),
+  created_at timestamptz NOT NULL DEFAULT now(),
+  updated_at timestamptz NOT NULL DEFAULT now(),
   CONSTRAINT FK_course__chapter FOREIGN KEY (id_chapter) REFERENCES chapter(id)
 );
-
