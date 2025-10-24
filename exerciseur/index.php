f9b973f08cb1cfc4960c428b3b9e805a1d55764f
Felix <bastien.felix@etu.univ-lyon1.fr>
2025-10-24 14:39:37 +0200

    footer en bas même quand la page est trop petite et index qui est beau

--- exerciseur/index.php
+++ exerciseur/index.php
@@ -9,33 +9,43 @@
 </head>
 <body>
     <?php include 'modules/header.php' ?>
-    <main class="home-page">
-        <div class="recent-exercise">
-            <h2>exercices récents</h2>
-            <a class="btn" href=""> xxxxxxxxxxxxxxxxxxxxxxxxx</a>
 
+    <main id="home-page">
+        <div class="recent-exercises">
+            <h2>Exercices récents</h2>
+            <a class="btn" href=""> xxxxxxxxxxxxxxxxxxxxxxxxx</a>
         </div>
+
         <div class="searching-section">
-            <form action="">
+            <form action="" method="post">
                 <div>
-                    <label for="exerciseSearchBar">Rechercher un exercice par mot clé</label>
-                    <input type="search" id="exerciseSearchBar" name="searchExercise" />
-                    <input type="submit">
+                    <label for="exerciseSearchBar"><h2>Rechercher un exercice par mot clé</h2></label>
+
+                    <div>
+                        <input type="search" id="exerciseSearchBar" name="searchExercise" class="btn">
+                        <input type="submit" class="btn">
+                    </div>
                 </div>
             </form>
-            <form
-                <div>
-                    <label for="classSearchBar">Rechercher une classe</label>
-                    <input type="search" id="classSearchBar" name="classSearchBar">
-                    <input type="submit">
-                </div>
 
+            <form action="" method="post">
+                <div>
+                    <label for="classSearchBar"><h2>Rechercher une classe</h2></label>
+
+                    <div>
+                        <input type="search" id="classSearchBar" name="classSearchBar" class="btn">
+                        <input type="submit" class="btn">
+                    </div>
+                </div>
+            </form>
         </div>
+
         <div class="popular-exercises">
-            <h2>exercices populaire</h2>
+            <h2>Exercices populaire</h2>
             <a class="btn" href=""> xxxxxxxxxxxxxxxxxxxxxxxxx</a>
         </div>
     </main>
+
     <?php include 'modules/footer.php' ?>
 </body>
 </html>
