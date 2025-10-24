e60ec6910b847ae927b11019c5614eb1c004fd40
p2411304 <jerome.tardivon@etu.univ-lyon1.fr>
2025-10-24 17:40:06 +0200

    secure comit

--- exerciseur/modules/header.php
+++ exerciseur/modules/header.php
@@ -3,15 +3,8 @@
 ?>
 <header>
     <div class="barHeader">
-<<<<<<< HEAD
         <img src="../exercisor3000.png" alt="">
         <h1><?php if(isset($_TITLE)){echo $_TITLE;}else{echo "Xercizor 3000";} ?></h1>
-=======
-        <a href="../index.php">
-            <img src="../exercisor3000.png" alt="">
-        </a>
-        <p>Exerciseur en ligne</p>
->>>>>>> e5eddf3d3adc570bcf1841a5b029227e9b775ed8
         <div class="buttonsHeader">
             <a class="btn" href="../teacher-space.php">Espace Professeurs</a>
             <a  class="btn" href="../profile.php">Profil</a>
