8bba151da5afb3d9de37679f8174735a7447f2f1
iutbgdin <iutbgdin@iutabgdinvm-63.iutbourg.univ-lyon1.fr>
2025-10-24 12:18:48 +0200

    séparation page connexion et inscription

--- exerciseur/login.php
+++ exerciseur/login.php
@@ -14,29 +14,6 @@
         <main>
             <form action="formlogin.php" method="post">
                 <fieldset>
-                    <legend>Inscription</legend>
-                    <fieldset>
-                        <legend>Status</legend>
-                        <input type="radio" name="status" value="teacher"><p>Enseignant(e)</p>
-                        <input type="radio" name="status" value="student"><p>Etudiant(e)</p>
-                    </fieldset>
-                    <fieldset>
-                        <legend>Identité</legend>
-                        <label  for="lastname">NOM</label>
-                        <input id="lastname" type="text" name="lastname">
-
-                        <label for="surname">Prénom</label>
-                        <input id="surname" type="text" name="surname">
-
-                        <label  for="email">adresse mail</label>
-                        <input id="email" type="email" name="email">
-
-                        <label for="password">Mot de passe</label>
-                        <input id="password" type="password" name="password">
-                    </fieldset>
-                </fieldset>
-
-                <fieldset>
                     <legend>Connexion</legend>
                     <label  for="email">Email</label>
                     <input id="email" type="email" name="email">
