9abc6a5aa0dd4e4ce6e79cfdf85aa2bcef8c1888
Samy Cuestas <samyfelipe90@hotmail.com>
2025-10-24 10:01:31 +0200

    Home page created

--- exerciseur/index.php
+++ exerciseur/index.php
@@ -9,7 +9,32 @@
 </head>
 <body>
     <?php include 'modules/header.php' ?>
-    <main>
+    <main class="home-page">
+        <div class="recent-exercise">
+            <h2>exercices récents</h2>
+            <a class="btn" href=""> xxxxxxxxxxxxxxxxxxxxxxxxx</a>
+
+        </div>
+        <div class="searching-section">
+            <form action="">
+                <div>
+                    <label for="exerciseSearchBar">Rechercher un exercice par mot clé</label>
+                    <input type="search" id="exerciseSearchBar" name="searchExercise" />
+                    <input type="submit">
+                </div>
+            </form>
+            <form
+                <div>
+                    <label for="classSearchBar">Rechercher une classe</label>
+                    <input type="search" id="classSearchBar" name="classSearchBar">
+                    <input type="submit">
+                </div>
+
+        </div>
+        <div class="popular-exercises">
+            <h2>exercices populaire</h2>
+            <a class="btn" href=""> xxxxxxxxxxxxxxxxxxxxxxxxx</a>
+        </div>
     </main>
     <?php include 'modules/footer.php' ?>
 </body>
--- exerciseur/modules/header.php
+++ exerciseur/modules/header.php
@@ -6,8 +6,8 @@
         <img src="exercisor3000.png" alt="">
         <p>Exerciseur en ligne</p>
         <div class="buttonsHeader">
-            <a class="btn" href="">Espace Professeurs</a>
-            <a  class="btn" href="">Profil</a>
+            <a class="btn" href="/../espace-professeur.php">Espace Professeurs</a>
+            <a  class="btn" href="/../profil.php">Profil</a>
         </div>
     </div>
     <nav id="menu">
