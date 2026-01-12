afd9efba2ad066a78d9188ef805e9c7be42c039c
Samy Cuestas <samyfelipe90@hotmail.com>
2026-01-12 10:21:32 +0100

    class and chapter search show message if they don't exist

--- exerciseur/processing-forms/processing-chapter-search.php
+++ exerciseur/processing-forms/processing-chapter-search.php
@@ -1,56 +1,44 @@
 <?php
 
+use db\Database;
+
 require_once __DIR__ . '/../db/db-connection.php';
 require_once __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../db/Database.php';
+$db = Database::getInstance();
 
 if (!isset($_GET["search"])) {
     $_GET["search"] = "";
 }
-
-$command = $db->prepare("SELECT title, description FROM chapter = id WHERE 
-                                (title LIKE concat('%', :title, '%') OR description LIKE concat('%', :title, '%')) AND visible = TRUE ");
-$command->execute([
-    "title" => $_GET["search"]
-]);
-
-$res = $command->fetchAll();
-
+$search = $_GET["search"];
+$res = $db->searchChapitreByTitleDesc($search);
 ?>
-
 <!DOCTYPE html>
-
 <html lang="fr">
-<?php $_TITLE="Recherche de Chapitre";include '../modules/include.php' ?>
+<?php $_TITLE = "Recherche de Chapitre";
+include '../modules/include.php' ?>
 
-    <body>
-        <!-- nav -->
-        <?php include '../modules/header.php' ?>
+<body>
+<!-- nav -->
+<?php include '../modules/header.php' ?>
+<main id="chapter-search">
+    <form action="/processing-forms/processing-chapter-search.php" method="get">
+        <div>
+            <label class="titleSearchBar" for="exerciseSearchBar">Rechercher un chapitre</label>
+            <div>
+                <input type="search" id="exerciseSearchBar" name="search" class="btn">
+                <button type="submit" class="btn">Rechercher</button>
+            </div>
+        </div>
+    </form>
+    <h2>Résultats</h2>
+    <?php if (count($res) == 0) { echo "<p>il n'y a aucun chapitre avec '$search'</p>";} ?>
+    <ol>
+        <?php foreach ($res as $r) { echo "<li>" . $r['title'] . " ; " . $r['description'] . "</li>"; } ?>
+    </ol>
+</main>
 
-        <main id="chapter-search">
-            <form action="/processing-forms/processing-chapter-search.php" method="get">
-                <div>
-                    <!-- @Bastien changement de h2 dans le label parce que c'est interdit ça bonne journée-->
-                    <label for="search"><h2>Rechercher un chapitre</h2></label>
-
-                    <div>
-                        <input type="search" id="exerciseSearchBar" name="search" class="btn">
-                        <button type="submit" class="btn">Rechercher</button>
-                    </div>
-                </div>
-            </form>
-
-            <h2>Résultats</h2>
-
-            <ol>
-                <?php
-                    foreach ($res as $r) {
-                        echo "<li>" . $r['title'] . " ; " . $r['description'] . "</li>";
-                    }
-                ?>
-            </ol>
-        </main>
-
-        <!-- footer -->
-        <?php include '../modules/footer.php' ?>
-    </body>
+<!-- footer -->
+<?php include '../modules/footer.php' ?>
+</body>
 </html>
