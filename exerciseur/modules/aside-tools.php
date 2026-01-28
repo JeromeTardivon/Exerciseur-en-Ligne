b546ba544258743622b061afeed1ff5032b85f9f
Samy Cuestas <samyfelipe90@hotmail.com>
2026-01-28 17:28:30 +0100

    sidebar in progress

---
+++ exerciseur/modules/aside-tools.php
@@ -0,0 +1,33 @@
+<div class = columnSideBar>
+    <button class="btn" id="button-tools">outils</button>
+    <div id="sidebar">
+        <aside id=chapter-creation-aside-1>
+            <h2>Outils</h2>
+            <div>
+                <div id="add-symbols-btn"></div>
+                <div id="symbols"></div>
+            </div>
+        </aside>
+        <aside id="chapter-creation-aside-2">
+            <h2>Modules</h2>
+            <form >
+                <button type="button" id="add-text">Ajouter un champ de texte</button>
+                <!-- show buttons if the span is clicked (and change image)-->
+                <input type="checkbox" id="dropdown" hidden/>
+                <label for="dropdown">Titres <img src="/Arrow-down.svg" alt="arrow" width="5px" height="5px"></label>
+                <div>
+                    <button type="button" id="add-title-1">Ajouter un titre 1</button>
+                    <button type="button" id="add-title-2">Ajouter un titre 2</button>
+                    <button type="button" id="add-title-3">Ajouter un titre 3</button>
+                    <button type="button" id="add-title-4">Ajouter un titre 4</button>
+                    <button type="button" id="add-title-5">Ajouter un titre 5</button>
+                </div>
+                <button type="button" id="add-multiple-choice">Ajouter une question QCM</button>
+                <button type="button" id="add-true-false">Ajouter une question Vrai/Faux</button>
+                <button type="button" id="add-open-question">Ajouter une question à réponse ouverte</button>
+                <button type="button" id="add-numerical-question">Ajouter une question numérique</button>
+                <button type="button" id="add-hint">Ajouter un indice</button>
+            </form>
+        </aside>
+    </div>
+</div>
